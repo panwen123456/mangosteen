fc339ccd9ec0a65eb359d0a9e49f85caf865892c
panwen123456 <a@b.com>
2022-12-15 17:05:41 +0800

    解决Edge浏览器一开始不能全屏的bug

--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,9 +1,19 @@
 import { defineComponent } from "vue";
-
+import { RouterLink } from "vue-router";
+import s from './First.module.scss';
 export const Second = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>Second</div>
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <div>second</div>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+          <RouterLink to="/welcome/2">下一页</RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </div>
+      </div>
     )
   },
 })
